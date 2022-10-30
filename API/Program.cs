7139df7d879954d209c73015af2eb8ae2148cb75
Ulises Lara <lara_ulises_@outlook.com>
2022-10-30 14:48:50 -0600

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+
+            using IServiceScope scope = host.Services.CreateScope();
+
+            IServiceProvider services = scope.ServiceProvider;
+
+            try
+            {
+                DataContext context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+
+                await Seed.SeedData(context);
+
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred during migration");
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
